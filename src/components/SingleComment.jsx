0881fe235fc3a1bbefa5d1f08f4f12655556ac1e
Constantin Negura <costelnegura1705@gmail.com>
2024-07-22 13:12:00 +0200

    first commit

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,39 @@
+import { Button, ListGroup } from 'react-bootstrap'
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
+        {
+          method: 'DELETE',
+          headers: {
+           'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNiZWRlZmRkN2I3MTAwMTRhMzEzYTYiLCJpYXQiOjE3MjA3MjE0OTQsImV4cCI6MTcyMTkzMTA5NH0.VjVUOuLT8Px1yEHTnkzDNilbmulXMkkCxYeF7yZWq3o' ,
+          },
+        }
+      )
+      if (response.ok) {
+        alert('La recensione è stata elimata!')
+      } else {
+        throw new Error('La recensione non è stata eliminata!')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  return (
+    <ListGroup.Item data-testid="single-comment">
+      {comment.comment}
+      <Button
+        variant="danger"
+        className="ms-2"
+        onClick={() => deleteComment(comment._id)}
+      >
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  )
+}
+
+export default SingleComment
